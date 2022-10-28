c6ffa2dc9499d87ae333700e94da40a4c0b28979
hsakhuja <hsakhuja@asu.edu>
2022-10-28 03:11:28 -0700

    Test code clean up

--- urinalsTest.java
+++ urinalsTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,73 +7,88 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class urinalsTest {
-    private urinals uri = new urinals();
+public class UrinalsTest {
+    private final urinals uri = new urinals();
+
     @Test
     void testGoodString() {
+        System.out.println("====== Hardik Sakhuja == TEST ONE EXECUTED =======");
+        System.out.println("testing Dummy test");
         // fail("Test Failed");
-        Assert.assertEquals(uri.goodString(""), true);
+        Assertions.assertEquals(uri.goodString(""), true);
     }
 
     @Test
     void testValid() {
-        List<String> testdata = List.of("011","10001000");
-        List<Boolean> answer = List.of(false,true); 
+        System.out.println("====== Hardik Sakhuja == TEST TWO EXECUTED =======");
+        System.out.println("Testing Valid and Invalid strings");
+        List<String> testdata = List.of("011", "10001000", "1000A001", "ASU", "1111111");
+        List<Boolean> answer = List.of(false, true, false, false, false);
         List<Boolean> result = new ArrayList<>();
-        for(int i=0; i<testdata.size();i++) {
-            result.add(uri.valid(testdata.get(i).trim().split("")));
+        for (String data : testdata) {
+            result.add(uri.valid(data.trim().split("")));
         }
-        Assert.assertEquals(answer, result);
+        Assertions.assertEquals(answer, result);
     }
 
     @Test
     void testGetFreeUrinals() {
-        List<String> testdata = List.of("10001","1001","00000","0000","01000","011");
-        List<Integer> answer = List.of(1,0,3,2,1,-1);
+        System.out.println("====== Hardik Sakhuja == TEST THREE EXECUTED =======");
+        System.out.println("testing getFreeUrinals Algorithm function");
+        List<String> testdata = List.of("10001", "1001", "00000", "0000", "01000", "011");
+        List<Integer> answer = List.of(1, 0, 3, 2, 1, -1);
         List<Integer> result = new ArrayList<>();
-        for(int i=0; i<testdata.size();i++) {
-            result.add(uri.getFreeUrinals(testdata.get(i).trim().split("")));
+        for (String data : testdata) {
+            result.add(uri.getFreeUrinals(data.trim().split("")));
         }
-        Assert.assertEquals(answer, result);
+        Assertions.assertEquals(answer, result);
     }
 
     @Test
-    void testReadSuccess() throws IOException{
-        String testFileName = new String();
-        List<String> answer = List.of("10001","1001","00000","0000","01000","011");
-        testFileName = "urinal.dat";
+    void testReadSuccess() throws IOException {
+        System.out.println("====== Hardik Sakhuja == TEST FOUR EXECUTED =======");
+        System.out.println("testing File read successfully");
+        List<String> answer = List.of("10001", "1001", "00000", "0000", "01000", "011");
+        String testFileName = "urinal.dat";
         List<String> result = uri.read(testFileName);
-        Assert.assertEquals(answer, result);
+        Assertions.assertEquals(answer, result);
     }
 
     @Test
     void testReadDoNotExist() {
+        System.out.println("====== Hardik Sakhuja == TEST FIVE EXECUTED =======");
+        System.out.println("testing read file - IOException and Do not Exist");
         String filename = "someFilename.txt";
         assertThrows(IOException.class, () -> uri.read(filename));
     }
 
     @Test
     void testReadEmptyFile() throws IOException {
+        System.out.println("====== Hardik Sakhuja == TEST SIX EXECUTED =======");
+        System.out.println("testing read file empty file");
         String testFilename = uri.generateFileName();
         List<String> emptydata = new ArrayList<>();
-        uri.write(emptydata,testFilename);
+        uri.write(emptydata, testFilename);
         List<String> readData = uri.read(testFilename);
-        Assert.assertEquals(readData,emptydata);
+        Assertions.assertEquals(readData, emptydata);
     }
+
     @Test
-    void testWrite() throws IOException{
-        String testFilename = new String();
-        List<String> testdata = List.of("10001","1001","00000","0000","01000","011");
-        testFilename = uri.generateFileName();
-        uri.write(testdata,testFilename);
+    void testWrite() throws IOException {
+        System.out.println("====== Hardik Sakhuja == TEST SEVEN EXECUTED =======");
+        System.out.println("testing write file successfully");
+        List<String> testdata = List.of("10001", "1001", "00000", "0000", "01000", "011");
+        String testFilename = uri.generateFileName();
+        uri.write(testdata, testFilename);
         List<String> readData = uri.read(testFilename);
-        Assert.assertEquals(readData,testdata);
+        Assertions.assertEquals(readData, testdata);
     }
 
     @Test
     void testWriteIOExcepton() {
+        System.out.println("====== Hardik Sakhuja == TEST EIGHT EXECUTED =======");
+        System.out.println("testing write IO Exception");
         final String filename = "/someDir/abc.txt";
-        assertThrows(IOException.class, () -> uri.write(new ArrayList<>(),filename));
+        assertThrows(IOException.class, () -> uri.write(new ArrayList<>(), filename));
     }
-
 }
