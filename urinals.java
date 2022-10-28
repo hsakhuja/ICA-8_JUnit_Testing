85adcfc46304850d0d84ba135a14adf8e4fac541
hsakhuja <hsakhuja@asu.edu>
2022-10-27 22:25:45 -0700

    add testcase to validate input string

--- urinals.java
+++ urinals.java
@@ -1,15 +1,24 @@
+
 /**
  * @author = Hardik Sakhuja (hsakhuja)
  * 
  */
 class urinals {
-
     public static void main(String args[]) {
         System.out.println("hello");
     }
 
-    public Boolean goodString( String str ) { // checks to see if valid string 
+    public boolean valid(String[] seq) {
+        for (int i = 0; i < seq.length - 1; i++) {
+            if (seq[i].equals("1") && seq[i + 1].equals("1")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Boolean goodString(String str) { // checks to see if valid string
         System.out.println("====== Hardik Sakhuja == TEST TWO EXECUTED =======");
-        return true; 
-   } 
+        return true;
+    }
 }
--- urinalsTest.java
+++ urinalsTest.java
@@ -1,13 +1,28 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class urinalsTest {
-
+    urinals urinal = new urinals();
     @Test
     void testGoodString() {
         // fail("Test Failed");
-        urinals urinal = new urinals();
         assertEquals(urinal.goodString(""), true);
+    }
+
+    @Test
+    void testValid() {
+        List<String> testdata = List.of("011","10001000");
+        List<Boolean> answer = List.of(false,true); 
+        List<Boolean> result = new ArrayList<>();
+        for(int i=0; i<testdata.size();i++) {
+            result.add(urinal.valid(testdata.get(i).trim().split("")));
+        }
+        Assert.assertEquals(answer, result);
     }
 }
